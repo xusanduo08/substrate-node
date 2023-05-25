eaba2b8f519d4974fc2db81ba98491aa6e0449b0
xusanduo08 <mengfansheng111@gmail.com>
2023-05-25 16:44:23 +0800

    撤销和转移存证

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -6,7 +6,8 @@ pub use pallet::*;
 pub mod pallet {
   use super::*;
 	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use frame_system::{pallet_prelude::*, ensure_signed};
+  use sp_std::prelude::*;
 
 
 	#[pallet::config] // 模块配置
@@ -48,6 +49,7 @@ pub mod pallet {
   #[pallet::call]
   impl<T: Config> Pallet<T> {
 
+    // 创建存证
     #[pallet::weight(0)]
     #[pallet::call_index(0)]
     pub fn create_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResult { // origin 交易的发送方，claim 存证
@@ -67,6 +69,36 @@ pub mod pallet {
       Self::deposit_event(Event::ClaimCreated(sender, claim));
 
       Ok(())
+    }
+
+    // 销毁存证
+    #[pallet::weight(1)]
+    pub fn revoke_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResult {
+      let sender = ensure_signed(origin)?;
+      let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone()).map_err(|_| Error::<T>::ClaimTooLong)?;
+      let (owner, _) = Proofs::<T>::get(&bounded_claim).ok_or(Error::<T>::ClaimNotExist)?; // 获取到这个claim的owner
+
+      // 校验交易的发送方和claim的owner一致
+      ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+      Proofs::<T>::remove(&bounded_claim);
+      Self::deposit_event(Event::ClaimRevoked(sender, claim));
+      Ok(().into())
+    }
+
+    // 转移存证
+    #[pallet::weight(2)]
+    pub fn transfer_claim(origin: OriginFor<T>, claim: Vec<u8>, dest: T::AccountId) -> DispatchResult {
+      // 验证发送方
+      let sender = ensure_signed(origin)?;
+
+      let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone()).map_err(|_| Error::<T>::ClaimTooLong)?;
+
+      let (owner, _) = Proofs::<T>::get(&bounded_claim).ok_or(Error::<T>::ClaimNotExist)?;
+      ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+      Proofs::<T>::insert(bounded_claim, (dest, frame_system::Pallet::<T>::block_number()));
+      Ok(().into())
     }
   }
 }
